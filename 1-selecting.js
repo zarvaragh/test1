d3595e9102d471a5650cc830c8bf23593711eeed
zarvaragh <ali.aghabeigiha@gmail.com>
2019-01-13 22:00:04 +0800

    selected added

---
+++ 1-selecting.js
@@ -0,0 +1,8 @@
+$("div").css("color", "white")
+$("div").css("background", 'purple')
+
+$(".highlight").css('width', '200px') // or can say $("div.highlight")
+
+$("#third").css("border", "2px solid orange")
+
+$("div:first-of-type").css("color", "pink")
